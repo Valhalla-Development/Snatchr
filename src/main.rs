c8f7240a57aab2924d69fc68a040124e1b57c327
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2025-07-27 18:13:23 +0100

    ✨ (main.rs): add tracing_subscriber to initialize logging with INFO level in main function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use tracing_subscriber;
+
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
     snatchr::run_server().await;
 }
