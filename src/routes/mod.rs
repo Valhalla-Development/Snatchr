cf7bfcc12107b2d6d159d8037e5bd39b5b12e98a
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2025-07-29 13:27:07 +0100

    ✨ (routes): introduce new files route to handle file serving in addition to the existing download route.

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,5 +2,7 @@
  * Module declaration for the routes.
  *
  * Contains the download route, which handles the download request.
+ * Contains the files route, which handles file serving.
  */
 pub mod download;
+pub mod files;
