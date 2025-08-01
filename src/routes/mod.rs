5351bfb5c4dd9d482127a2949857cd59345525a9
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2025-08-01 13:14:43 +0100

    ✨ (routes): add new page route to serve the HTML download page.

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -4,7 +4,9 @@
  * Contains the download route, which handles the download request.
  * Contains the files route, which handles file serving.
  * Contains the health route, which handles health checks.
+ * Contains the page route, which serves the HTML download page.
  */
 pub mod download;
 pub mod files;
 pub mod health;
+pub mod page;
